9702536816e3235a49312d753a25d25d9aef4ea3
LamEr <cesar_silva_1995@hotmail.com>
2017-06-11 21:17:20 +0100

    cenas po bernardo

--- about.c
+++ about.c
@@ -16,9 +16,9 @@ int main() {
 		printf("<text x=285 y=20 fill = \"#C4C3C3\" text-anchor = \"middle\" text-align = \"center\" font-family = \"serif\">Jogo inspirado num coffee break roguelike desenvolvido no ambito da</text>\n");
 		printf("<text x=115 y=40 fill = \"#C4C3C3\" text-anchor = \"middle\" text-align = \"center\" font-family = \"serif\">cadeira de LA no ano 2017.</text>\n");
 
-		printf("<text x=90 y=80 fill = \"#C4C3C3\" text-anchor = \"middle\" text-align = \"center\" font-family = \"serif\">Cesar Silva      -     A79008</text>\n");
+		printf("<text x=90 y=80 fill = \"#C4C3C3\" text-anchor = \"middle\" text-align = \"center\" font-family = \"serif\">Cesar Silva      -     A77518</text>\n");
 		printf("<text x=125 y=100 fill = \"#C4C3C3\" text-anchor = \"middle\" text-align = \"center\" font-family = \"serif\">Bernardo Rodrigues - A79008</text>\n");
-		printf("<text x=100 y=120 fill = \"#C4C3C3\" text-anchor = \"middle\" text-align = \"center\" font-family = \"serif\">Tigas Pereira    -   A79008</text>\n");
+		printf("<text x=100 y=120 fill = \"#C4C3C3\" text-anchor = \"middle\" text-align = \"center\" font-family = \"serif\">Tigas Pereira    -   A77504</text>\n");
 
 		printf("<text x=285 y=160 fill = \"#C4C3C3\" text-anchor = \"middle\" text-align = \"center\" font-family = \"serif\">Carrega nas casas a tua volta para te movimentares se carregares em</text>\n");
 		printf("<text x=1 y=180 fill = \"#C4C3C3\" font-family = \"serif\">cima de um monstro vais mata-lo mas cuidado pois se te movimentares</text>\n");
